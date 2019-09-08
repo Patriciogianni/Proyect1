53804a867e9f5026650591f95c2a40f711c68f14
Patricio <giannipato@gmail.com>
2019-09-08 07:45:24 -0300

    Proyect ready to be reviewed

---
+++ Proyecto1/Domain/Person.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1.Domain
+{
+    [Serializable]
+    public class Person
+    {
+        private int du;
+        private string gender;
+        private string lastName;
+        private string name;
+        private int age;
+        private string dateBirth;
+        private string codePerson;
+        public int Du
+        {
+            get
+            {
+                return du;
+            }
+
+            set
+            {
+                du = value;
+            }
+
+        }
+        public string Gender
+        {
+            get
+            {
+                return gender;
+            }
+
+            set
+            {
+                gender = value;
+            }
+
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                lastName = value;
+            }
+
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+
+        }
+
+        public string DateBirth
+        {
+            get
+            {
+                return dateBirth;
+            }
+
+            set
+            {
+                dateBirth = value;
+            }
+
+        }
+
+        public string CodePerson
+        {
+            get
+            {
+                return codePerson;
+            }
+
+            set
+            {
+                codePerson = value;
+            }
+
+        }
+
+        public int generateCodePerson()
+        {
+            int code;
+            Random random = new Random();
+            code = random.Next(100, 1000);
+            return code;
+        }
+
+        public void showPerson() // Muestro por patallla lo cargado.
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Estos son los Datos Cargados...");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("DU: {0}\n", Du);
+            Console.WriteLine("Genero: {0}\n", Gender);
+            Console.WriteLine("Apellido: {0}\n", LastName);
+            Console.WriteLine("Nombre: {0}\n", Name);
+            Console.WriteLine("Edad: {0}\n", Age);
+            Console.WriteLine("Fecha de Nacimiento: {0}\n", DateBirth);
+            Console.WriteLine("Codigo de la persona : {0}\n", CodePerson);
+            Console.WriteLine("Se ha guardado la persona en la base de datos\n");
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
---
+++ Proyecto1/Domain/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1.Domain
+{
+    class User
+    {
+        private string name;
+        private string password;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+    }
+}
